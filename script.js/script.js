1acdfeaa834b7b2bcf4de56c839bb45f601c954e
Thiana <thianacantunes@gmail.com>
2023-01-19 16:56:22 -0300

    versão com enviar mensagem finalizado

--- script.js/script.js
+++ script.js/script.js
@@ -1,64 +1,124 @@
 
-const nome = prompt('Qual o seu nome?');
+let nome = prompt('Qual o seu nome?');
 
-let nomeUsuario = 
+const nomeUsuario = 
     {
         name: nome
     }
 
+let batePapo = document.querySelector('.areaConversa');
+const mensagemDigitada = document.querySelector('.boxLow input');
+
+   
+
+verificarUsuario();
 
 
-function abrirAbaLateral() {
-    const abaLateral = document.querySelector('.overlay');
-    abaLateral.classList.remove('escondido');
+
+function verificarUsuario(){
+    
+    const resposta = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeUsuario);
+
+    resposta
+    .then(usuarioVerificado)
+
 }
-
-function entrarNaSala(){
-
-    console.log(axios);
-
-    const conversa = document.querySelector('.areaConversa');
-    conversa.innerHTML = '';
-
-
-        let template = `
-            <div class="entraNaSala">
-                (09:21:45) ${nomeUsuario.name} entra na sala...
-            </div> <!-- fechamento entra na sala-->
-        `;
-
-        conversa.innerHTML += template;
-    }
-
-
-
-const resposta = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeUsuario);
-
-resposta.then(entrarNaSala);
 
 function mantendoConexao(){
 
+    
+
 const manterConexao = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nomeUsuario);
 
-manterConexao.then(respostaChegou)
+manterConexao.then()
 manterConexao.catch()
 
-
-
-
 }
-setInterval(mantendoConexao, 5000);
+
+
+
 
 
 function respostaChegou(resposta){
-    
-    console.log('resposta chegou!!!!');
-    console.log(resposta);
+
+
+
    
-}
+    let batePapo = document.querySelector('.areaConversa');
+    batePapo.innerHTML = '';
 
-function deuRuim (){
+        for (let i = 0; i < 100; i++){
+           
+
+            let hora = resposta.data[i].time;
+            let nome1 = resposta.data[i].from;
+            let nome2 = resposta.data[i].to;
+            let texto = resposta.data[i].text;
+            let tipo = resposta.data[i].type;
+
+            batePapo.innerHTML += `
+            
+            <div class="${tipo}">
+                (${hora}) ${nome1} para ${nome2}: ${texto}
+            </div> <!-- fechamento entra na sala -->
+        
+
+            `;
+        }
+
+        batePapo.querySelector('div:last-child').scrollIntoView();
+    }
+
+
+function deuRuim (erro){
     console.log('deu ruim');
+    console.log(erro);
+}
+
+
+
+function pegarConversaNoServidor(){
+
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+
+    promessa.then(respostaChegou);
+    promessa.catch(deuRuim);
+
 
 }
 
+
+
+
+
+function enviarMensagem(){
+
+    const msg = {
+        from: nome,
+        to: "Todos",
+        text: mensagemDigitada.value,
+        type: "message" 
+    }
+
+    const enviar = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', msg);
+
+    enviar.then(pegarConversaNoServidor);
+    enviar.catch(deuRuim);
+}
+
+
+
+
+
+
+function usuarioVerificado (){
+   
+    
+
+        pegarConversaNoServidor()
+        setInterval(function (){ 
+            pegarConversaNoServidor()
+        } ,3000)
+        
+        setInterval(mantendoConexao, 5000);
+}
